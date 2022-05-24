3549634ca766a3dd35978b13773fb0d9526b060a
Hafidz25 <hafidzalif80@gmail.com>
2022-05-24 09:47:26 +0700

    backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd2969ce74faa10503ca63d2eeca5f865'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9af22de7-ae32-403c-8e23-a7d7a74b0f8e'
+};
